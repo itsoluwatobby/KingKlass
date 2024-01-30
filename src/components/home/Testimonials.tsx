1959ac4ccf60d7126f9f449cf653f7a1acce4420
itsoluwatobby <oluwasamuel050@gmail.com>
2024-01-30 18:42:04 +0100

    continuation

--- src/components/home/Testimonials.tsx
+++ src/components/home/Testimonials.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import { DesignImage } from '../appComponents/Image'
+import { setCustomBackgroundImage } from '../../utility/setBackGroundImage'
 
-type Props = {}
 
-export default function Testimonials({}: Props) {
+export default function Testimonials() {
   return (
-    <section className='flex flex-col w-full'>
+    <section className='flex flex-col w-full pt-1 pb-4'>
       <div className='relative w-full p-4 flex flex-col items-center bg-[#311807]'>
-        <DesignImage 
-          image='/quotes.png' rounded=''
-          classNames='absolute'
-        />
+        <div 
+        style={setCustomBackgroundImage(
+          '/quotes.png',
+          {
+            backgroundSize: '15%',
+            backgroundPosition: '10%',
+          }
+        )}
+        className='flex w-full z-20 text-white items-end flex-col gap-y-4'>
+          <h2 className='w-fit text-white text-3xl'>Our Happy Clients</h2>
+          <p className='text-xs'>What Our Clients Say About Theur Tailoring Experience</p>
+        </div>
       </div>
     </section>
   )
