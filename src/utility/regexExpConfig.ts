2c1fc0d81675d491270daa75fb89eeb437d9cef5
Oluwatobi Akinola Samuel <oluwasamuel050@gmail.com>
2024-02-03 08:26:11 +0100

    Update regexExpConfig.ts

--- src/utility/regexExpConfig.ts
+++ src/utility/regexExpConfig.ts
@@ -1,4 +1,4 @@
 export const Config_RegExp = {
   "EMAIL_REG": /^[\d\w]{1,55}@[\w]{1,10}\.[\w]{1,5}$/,
-  "PASS_REG": ""
+  "PASS_REG": ^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!£%*?&\\\/])[A-Za-z\d@£$!%*?&]{9,60}$
 }
